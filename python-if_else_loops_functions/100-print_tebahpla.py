467970f467f4889d1780a5cfc36fcdf184b6f3cd
Semedw <smusazade2000@gmail.com>
2025-11-27 18:39:05 +0400

    task 13

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,6 @@
 
 for i in range(ord('z'), ord('a')-1, -1):
     if i%2 == 0:
-        print(chr(i), end='')
+        print("{}".format(chr(i)), end='')
     else:
-        print(chr(i-32), end='')
+        print("{}".format(chr(i-32)), end='')
