916975080f63f73acf280230bc21b43405c22f9d
Semedw <smusazade2000@gmail.com>
2025-12-01 17:58:42 +0400

    task 4 final

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(mylist, idx, element):
-    if idx < 0 and idx >= len(mylist):
+    if idx < 0 or    idx >= len(mylist):
         return mylist
     new_list = mylist.copy()
     new_list[idx] = element
