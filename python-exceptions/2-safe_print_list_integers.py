47bd3a16a442bd82f9deaac85866af7e0a380812
Semedw <smusazade2000@gmail.com>
2025-12-02 17:58:39 +0400

    task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,5 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
             s += 1
         except ValueError:
             pass
+        except TypeError:
+            pass
     print()
     return s
