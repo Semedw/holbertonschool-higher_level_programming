33fdfb5f7adfd8292547bafe8c9f53651863a3e9
Semedw <smusazade2000@gmail.com>
2025-12-02 09:44:29 +0400

    task7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 def add_tuple(a, b):
-    tuple1 = []
-    tuple1.append(list(a)[0]+list(b)[0])
-    tuple1.append(list(b)[1]+list(a)[1])
-    tuple1 = tuple(tuple1)
-    return tuple1
+    a = list(a)
+    b = list(b)
+    if len(a) < 2:
+        if len(a) == 1:
+            a.append(0)
+        else:
+            a.append(0)
+            a.append(0)
+    if len(b) < 2:
+        if len(b) == 1:
+            b.append(0)
+        else:
+            b.append(0)
+            b.append(0)
+    new_list = []
+    for i in range(2):
+        new_list.append(a[i] + b[i])
+
+    return tuple(new_list)
+    
