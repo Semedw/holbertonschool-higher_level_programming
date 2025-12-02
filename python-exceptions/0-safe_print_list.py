f890c790e8a9e0013adec3339675e55dc4abbec5
Semedw <smusazade2000@gmail.com>
2025-12-02 17:29:14 +0400

    task 0 I hope this works

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,9 +3,12 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end='')
+        print()
         return x
     except IndexError:
         s = 0
         for i in my_list:
+            print(i, end='')
             s += 1
+        print()
         return s
