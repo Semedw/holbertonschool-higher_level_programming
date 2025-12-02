033869c20352e82697d9b5b5ff300295b54b0906
Semedw <smusazade2000@gmail.com>
2025-12-02 17:04:17 +0400

    task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+numerals = {
+    'I' : 1,
+    'V' : 5,
+    'X' : 10,
+    'L' : 50,
+    'C' : 100,
+    'D' : 500,
+    'M' : 1000
+}
+
+def roman_to_int(roman_string):
+    s = 0
+    l = []
+    for i in roman_string:
+        l.append(numerals[i])
+    for i in range(len(l)-1):
+        if l[i] < l[i+1]:
+            l[i] = -l[i]
+    return sum(l)
