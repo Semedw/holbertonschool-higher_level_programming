fa22e53934f5f3ee6b2c316cd6116ce73a96a771
Semedw <smusazade2000@gmail.com>
2025-12-02 17:10:34 +0400

    pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 numerals = {
-    'I' : 1,
-    'V' : 5,
-    'X' : 10,
-    'L' : 50,
-    'C' : 100,
-    'D' : 500,
-    'M' : 1000
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
 }
+
 
 def roman_to_int(roman_string):
     try:
