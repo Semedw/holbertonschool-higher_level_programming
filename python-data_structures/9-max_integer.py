1c0174a04d562375823960b8e0d4f4d2f5d7cf61
Semedw <smusazade2000@gmail.com>
2025-12-02 09:51:06 +0400

    task 9 final

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,10 +3,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    
     k = my_list[0]
     for i in my_list:
         if i > k:
-            k = i
-        
+            k = i   
     return k
