9f9cf414ef5118396e88c409d2d30ebeb4255fa8
Semedw <smusazade2000@gmail.com>
2025-12-02 10:21:47 +0400

    task 0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        k = []
+        for j in i:
+            k.append(j**2)
+        new_matrix.append()
+    return new_matrix
