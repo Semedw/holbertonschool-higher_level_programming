70b7fd64f2ec73c732bb7ea1db2a5ddd25ca6b47
Semedw <smusazade2000@gmail.com>
2025-12-02 09:54:49 +0400

    task 10

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def divisible_by_2(mylist):
+    result = []
+    for i in mylist:
+        if i % 2 == 0:
+            result.append(True)
+        else:
+            return False
+    return result
