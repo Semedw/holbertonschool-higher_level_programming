002e06d3aab803cc2eb15f88de398adcc8d69456
Semedw <smusazade2000@gmail.com>
2025-12-03 12:31:26 +0400

    error handling

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,23 +5,22 @@ if __name__ == '__main__':
     argv = sys.argv[1:]
     if len(argv) != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        print(1)
-    
-    operators = [
-        '+',
-        '-',
-        '*',
-        '/'
-    ]
-    if argv[1] not in operators:
-        print('Unknown operator. Available operators: +, -, * and /')
-        print(1)
-    
-    if argv[1] == '+':
-        print('{} + {} ='.format(argv[0], argv[2]), add(int(argv[0]), int(argv[2])))
-    if argv[1] == '-':
-        print('{} - {} ='.format(argv[0], argv[2]), sub(int(argv[0]), int(argv[2])))
-    if argv[1] == '*':
-        print('{} * {} ='.format(argv[0], argv[2]), mul(int(argv[0]), int(argv[2])))
-    if argv[1] == '/':
-        print('{} / {} ='.format(argv[0], argv[2]), div(int(argv[0]), int(argv[2])))
+    else:
+        operators = [
+            '+',
+            '-',
+            '*',
+            '/'
+        ]
+        if argv[1] not in operators:
+            print('Unknown operator. Available operators: +, -, * and /')
+            print(1)
+        
+        if argv[1] == '+':
+            print('{} + {} ='.format(argv[0], argv[2]), add(int(argv[0]), int(argv[2])))
+        if argv[1] == '-':
+            print('{} - {} ='.format(argv[0], argv[2]), sub(int(argv[0]), int(argv[2])))
+        if argv[1] == '*':
+            print('{} * {} ='.format(argv[0], argv[2]), mul(int(argv[0]), int(argv[2])))
+        if argv[1] == '/':
+            print('{} / {} ='.format(argv[0], argv[2]), div(int(argv[0]), int(argv[2])))
