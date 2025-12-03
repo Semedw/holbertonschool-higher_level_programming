fe79f30785fc630cffa84e27154c9d77d6add8bc
Semedw <smusazade2000@gmail.com>
2025-12-03 12:33:14 +0400

    adding exit

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     argv = sys.argv[1:]
     if len(argv) != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        print(1)
+        exit(1)
     else:
         operators = [
             '+',
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         ]
         if argv[1] not in operators:
             print('Unknown operator. Available operators: +, -, * and /')
-            print(1)
+            exit(1)
         
         if argv[1] == '+':
             print('{} + {} ='.format(argv[0], argv[2]), add(int(argv[0]), int(argv[2])))
