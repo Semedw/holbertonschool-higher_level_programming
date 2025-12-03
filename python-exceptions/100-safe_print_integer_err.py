7fde439f04882ec154dbe6ec3f4cf435f02b1dd7
Semedw <smusazade2000@gmail.com>
2025-12-03 14:03:18 +0400

    task 100 final

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -3,6 +3,7 @@ def safe_print_integer_err(value):
     import sys
     try:
         print("{:d}".format(value))
+        return True
     except Exception as e:
         print(f"Exception: {e}", file=sys.stderr)
         return False
