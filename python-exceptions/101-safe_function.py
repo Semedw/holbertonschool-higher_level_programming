f413230debdc470c9ac99d7003521acd6acb7cb4
Semedw <smusazade2000@gmail.com>
2025-12-03 14:09:34 +0400

    task 101

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -2,7 +2,7 @@
 def safe_function(fct, *args):
     import sys
     try:
-        return fct
+        return fct(*args)
     except Exception as e:
         print(f"Excemption: {e}", file=sys.stderr)
         return None
