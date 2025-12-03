08607b7dbf9300e684f0c6ccd45bde4d52b56dd0
Semedw <smusazade2000@gmail.com>
2025-12-03 14:02:49 +0400

    task 100

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+    except Exception as e:
+        print(f"Exception: {e}", file=sys.stderr)
+        return False
