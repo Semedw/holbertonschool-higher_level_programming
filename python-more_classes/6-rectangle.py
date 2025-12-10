1954fba37e0e431381ec9ee33ce17ab83fc256a5
Semedw <smusazade2000@gmail.com>
2025-12-10 22:05:58 +0400

    defining class attribute

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -9,13 +9,14 @@ class Rectangle:
     Inside the rectangle class
     """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """
         Initialize when object is created
         """
         self.width = width
         self.height = height
-        number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -92,4 +93,4 @@ class Rectangle:
         Executed when the object is deleted
         """
         print("Bye rectangle...")
-        number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
