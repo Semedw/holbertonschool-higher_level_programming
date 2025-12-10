861572f8a3bd99921988daef2c42c82cda680717
Semedw <smusazade2000@gmail.com>
2025-12-10 21:52:12 +0400

    bye rectangle

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+"""
+Rectangle class
+"""
+
+
+class Rectangle:
+    """
+    Inside the rectangle class
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Initialize when object is created
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        Property of width
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """
+        Property of height
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """
+        Width setter
+        """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """
+        Height setter
+        """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
+
+    def area(self):
+        """
+        This method calculates area
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        This method returns the perimeter of rectangle
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        """
+        Printing rectangle
+        """
+        if self.__height == 0 or self.__width == 0:
+            return ''
+        s = ''
+        for i in range(self.__height):
+            s += "#" * self.__width + '\n'
+
+        return s.strip()
+
+    def __repr__(self):
+        """
+        Representation of rectangle
+        """
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        """
+        Executed when the object is deleted
+        """
+        return "Bye rectangle..."
