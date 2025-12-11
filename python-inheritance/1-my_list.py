47545f923dcb8381de4f6018d152bc42f6567cbf
Semedw <smusazade2000@gmail.com>
2025-12-11 18:21:57 +0400

    inheriting list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Uzeyir menim canimdi
+"""
+
+
+class MyList(list):
+    """
+    inside the mylist class
+    """
+    def print_sorted(self):
+        """
+        sorting the elements of the list
+        """
+        print(sorted(list))
