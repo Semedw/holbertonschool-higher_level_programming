2f4503084db4b254531adb0a0fa062371b6b0675
Semedw <smusazade2000@gmail.com>
2025-12-12 11:25:11 +0400

    variable name error

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,4 +10,4 @@ def inherits_from(obj, a_class):
     """
     if obj.__class__ == a_class:
         return False
-    return issubclass(oj.__class__, a_class)
+    return issubclass(obj.__class__, a_class)
