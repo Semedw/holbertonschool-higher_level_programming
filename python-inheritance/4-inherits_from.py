61eea614070c935802c0e2db46636a90cad82cca
Semedw <smusazade2000@gmail.com>
2025-12-12 11:19:36 +0400

    only subclass

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,7 +10,7 @@ def inherits_from(obj, a_class):
     """
     if isinstance(obj.__class__, a_class):
         subcls = obj.__class__
-        if isinstance(subcls, a_class):
+        if issubclass(subcls, a_class):
             return True
         return False
     return False
