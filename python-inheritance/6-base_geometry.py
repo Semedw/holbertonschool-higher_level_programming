fce412bfcaba021b5aa4c80d4e607859287eee17
Semedw <smusazade2000@gmail.com>
2025-12-12 11:30:03 +0400

    base geometry raisin exeption

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+empty class
+"""
+
+
+class BaseGeometry:
+    """
+    inside the base geometry class
+    """
+
+    def area(self):
+        """
+        raises an exeption
+        """
+        raise Exception('area() is not implemented)
