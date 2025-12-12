8a5bd19b8ee0e9b8c7a363414aca502604c7fb0d
Semedw <smusazade2000@gmail.com>
2025-12-12 11:14:29 +0400

    is exactly the instance of the class

---
+++ python-inheritance/2-is_some_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+is same class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    check if the object is instance of the given class
+    """
+    return isinstance(obj, a_class)
