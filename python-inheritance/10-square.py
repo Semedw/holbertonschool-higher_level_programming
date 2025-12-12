7e0502b31b1db0ea6b15e97fbcde1fca09653f77
Semedw <smusazade2000@gmail.com>
2025-12-12 17:02:30 +0400

    task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+sqaure that was inherited from rectangle
+"""
+
+Rectangle = __import__("9-rectangle.py").Recctangle
+
+
+class Square(Rectangle):
+    """
+    inside the square
+    """
+    
+    def __init__(self, size):
+        """
+        inside the init method
+        """
+        self.integer_validator("size", size)
+
+        self.__size = size
