d26780fbaca55b9e54740d40d48888202e498766
Semedw <smusazade2000@gmail.com>
2025-12-12 11:16:31 +0400

    changing the function

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -8,4 +8,6 @@ def is_same_class(obj, a_class):
     """
     check if the object is instance of the given class
     """
-    return isinstance(obj, a_class)
+    if obj.__class__ == a_class:
+        return True
+    return False
