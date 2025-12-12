cb7460e291b607cac3f5f7869259ccd7117404fe
Semedw <smusazade2000@gmail.com>
2025-12-12 17:10:03 +0400

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+sqaure that was inherited from rectangle
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    inside the square
+    """
+
+    def __init__(self, size):
+        """
+        inside the init method
+        """
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        print(f'[Square] {size}/{size}')
