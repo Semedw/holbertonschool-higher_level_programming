1e3cc55b9fe533d447f1864c2cebfc70471b1d98
Semedw <smusazade2000@gmail.com>
2025-12-13 14:08:54 +0400

    changed write to append

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -10,5 +10,5 @@ def save_to_json_file(my_obj, filename):
     """
     inside the function
     """
-    with open(filename, mode='w', encoding='utf-8') as f:
+    with open(filename, mode='a', encoding='utf-8') as f:
         text = json.dump(my_obj, f)
