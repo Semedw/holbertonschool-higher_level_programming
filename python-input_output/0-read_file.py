05fbf1f9bc8ae438d2575629b22f44a144c951bb
Semedw <smusazade2000@gmail.com>
2025-12-13 12:40:25 +0400

    test 2

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,6 +9,6 @@ def read_file(filename=''):
     inside the function
     """
     with open(filename, encoding='utf-8') as f:
-        read_text = f.read()
+        read_text = f.readlines()
         for line in read_text:
             print(line)
