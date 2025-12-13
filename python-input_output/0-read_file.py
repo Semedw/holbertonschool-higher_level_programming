afc03631b5a2db5e48d1b11f6b7ff57f45f695b5
Semedw <smusazade2000@gmail.com>
2025-12-13 12:29:46 +0400

    changin the structure

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=''):
     inside the function
     """
     with open(filename, encoding='utf-8') as f:
-        read_text = f.read()
-        print(read_text.strip())
+        for line in f:
+            print(line.strip())
