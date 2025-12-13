9aa7c7ff33cd85bbdbebc70e6512916a30a9a4f2
Semedw <smusazade2000@gmail.com>
2025-12-13 14:14:41 +0400

    added load

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,5 +11,5 @@ save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
 argv = (sys.argv[1:])
-load = load_from_json_file('add_item.json')
+load('add_item.json')
 save(argv, 'add_item.json')
