311a8552202c392cfaafb4b774625a1737b00095
Semedw <smusazade2000@gmail.com>
2025-12-13 14:04:18 +0400

    test 2

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,4 +11,4 @@ save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
 argv = sys.argv[1:]
-save(argv)
+save('add_time.json', argv)
