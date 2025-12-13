618da65a7feced25fe92092d9682f862de01cba0
Semedw <smusazade2000@gmail.com>
2025-12-13 13:06:38 +0400

    documenting the function

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,5 +5,8 @@ append to a file
 
 
 def append_write(filename='', text=''):
+    """
+    inside the function
+    """
     with open(filename, mode='a', encoding='utf-8') as f:
         return f.write(text)
