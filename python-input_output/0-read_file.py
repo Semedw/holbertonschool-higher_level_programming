694ad0574fc08478993efd5b7a61fa73c0652f55
Semedw <smusazade2000@gmail.com>
2025-12-13 12:18:50 +0400

    reading file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+reading a text file
+"""
+
+
+def read_file(filename=''):
+    """
+    inside the function
+    """
+    with open(filename, encoding='utf-8') as f:
+        f.read()
