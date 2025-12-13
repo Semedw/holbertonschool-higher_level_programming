879a408fe141c3454f5e0c876bb1fa17a2baebc4
Semedw <smusazade2000@gmail.com>
2025-12-13 23:27:59 +0400

    update json

---
+++ python-input_output/11-student.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+student to json
+"""
+
+
+class Student:
+    """
+    inside the student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        inside the init method
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        inside the public function
+        """
+        d_json = {}
+        result = {}
+        s = 0
+        d_json['first_name'] = self.first_name
+        d_json['last_name'] = self.last_name
+        d_json['age'] = self.age
+        if attrs is None:
+            return d_json
+        for key, value in d_json.items():
+            if key in attrs:
+                result[key] = value
+        return result
+    
+    def reload_from_json(self, json):
+        """
+        inside the method
+        """
+        updated_json = {}
+        for key, value in json.items():
+            updated_json[key] = value
+        return updated_json
