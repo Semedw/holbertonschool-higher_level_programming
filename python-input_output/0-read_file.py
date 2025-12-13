5ea8bc4fa0e971be64daff8e941800a2ee7193b8
Semedw <smusazade2000@gmail.com>
2025-12-13 12:43:06 +0400

    end=""

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,6 +9,5 @@ def read_file(filename=''):
     inside the function
     """
     with open(filename, encoding='utf-8') as f:
-        read_text = f.readlines()
-        for line in read_text:
-            print(line)
+        read_text = f.read()
+        print(read_text, end='')
