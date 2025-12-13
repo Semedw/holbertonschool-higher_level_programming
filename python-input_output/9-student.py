c2d7e3ca6240f40c7a76c658cf27b629d3eabb88
Semedw <smusazade2000@gmail.com>
2025-12-13 23:06:55 +0400

    changed the value

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -23,6 +23,6 @@ class Student:
         """
         d_json = {}
         d_json['first_name'] = self.first_name
-        d_json['last_name'] = self.first_name
+        d_json['last_name'] = self.last_name
         d_json['age'] = self.age
         return d_json
