086ac19ddace930a29feaa3a975db304719f9f97
Semedw <smusazade2000@gmail.com>
2025-12-13 14:28:34 +0400

    added str to path

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ from pathlib import Path
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
-if Path(add_item.json).exists():
+if Path('add_item.json').exists():
     argv = load('add_items.json')
 else:
     argv = []
