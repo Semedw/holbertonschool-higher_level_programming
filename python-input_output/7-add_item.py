22ce690cb970e8dd9167f9b111b44fe3d521efee
Semedw <smusazade2000@gmail.com>
2025-12-13 14:27:10 +0400

    added pathlib

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,15 +3,16 @@
 load, add, and save
 """
 
-import json
 import sys
-
+from pathlib import Path
 
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
-argv = []
-try:
-    argv += load('add_item.json')
-except FileNotFoundError:
-    argv = sys.argv[1:]
+
+if Path(add_item.json).exists():
+    argv = load(add_items.json)
+else:
+    argv = []
+
+argv += sys.argv[1:]
 save(argv, 'add_item.json')
