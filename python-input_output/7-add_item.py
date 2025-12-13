9f1fb644a82f0b791cfeb07a06d86ff074ad2866
Semedw <smusazade2000@gmail.com>
2025-12-13 14:23:17 +0400

    added try except structure

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,6 +10,8 @@ import sys
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
-argv = (sys.argv[1:])
+try:
+    argv += load('add_item.json')
+except FileNotFoundError:
+    argv = sys.argv[1:]
 save(argv, 'add_item.json')
-load('add_item.json')
