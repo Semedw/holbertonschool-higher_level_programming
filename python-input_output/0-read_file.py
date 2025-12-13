b05705552c3c0611e5533f8db461b0d0206d09db
Semedw <smusazade2000@gmail.com>
2025-12-13 12:26:49 +0400

    strip

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -11,4 +11,4 @@ def read_file(filename=''):
     with open(filename, encoding='utf-8') as f:
         line_list = f.readlines()
         for line in line_list:
-            print(line)
+            print(line.strip())
