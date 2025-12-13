9c051eb2721adc66cd11a225b4853ad56dbc6352
Semedw <smusazade2000@gmail.com>
2025-12-13 12:53:56 +0400

    changing back

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,5 +8,5 @@ def write_file(filename='', text=''):
     """
     inside the function
     """
-    with open(filename, mode='r+', encoding='utf-8') as f:
+    with open(filename, mode='w', encoding='utf-8') as f:
         f.write(text)
