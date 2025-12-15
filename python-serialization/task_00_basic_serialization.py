a7a323ba531f79fa66af42c7d553e09aca429420
Semedw <smusazade2000@gmail.com>
2025-12-15 18:15:41 +0400

    removed encoding

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -9,7 +9,7 @@ def serialize_and_save_to_file(data, filename):
     """
     inside the serialize function
     """
-    with open(filename, mode='wb', encoding='utf-8') as file:
+    with open(filename, mode='wb') as file:
         pickle.dump(data, filename)
 
 
@@ -17,6 +17,6 @@ def load_and_deserialize(filename):
     """
     inside the load function
     """
-    with open(filename, mode='rb', encoding='utf-8') as file:
+    with open(filename, mode='rb') as file:
         result = pickle.load(filename)
     return result
