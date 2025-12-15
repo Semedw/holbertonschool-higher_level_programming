1eaf7fd9af88479f2c9972f76919cbc6a112d4a1
Semedw <smusazade2000@gmail.com>
2025-12-15 18:46:42 +0400

    task 01 initial

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+pickling custom classes
+"""
+
+
+class CustomObject:
+    """
+    inside the class
+    """
+
+    def __init__(self, name:str, age:int, is_student:bool):
+        """
+        inside the init file
+        """
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self, name, age, is_stuent):
+        """
+        inside the display func
+        """
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
+
+    def serialize(self, filename):
+        with open(filename, 'wb') as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, 'rb') as file:
+            deserialized_list = pickle.load(file)
+        return deserialized_list
