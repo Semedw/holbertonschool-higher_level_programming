928a72428cba56fd4194349a8f30aa5bffea81d9
Semedw <smusazade2000@gmail.com>
2025-12-15 18:17:55 +0400

    removed mode

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -9,7 +9,7 @@ def serialize_and_save_to_file(data, filename):
     """
     inside the serialize function
     """
-    with open(filename, mode='wb') as file:
+    with open(filename, 'wb') as file:
         pickle.dump(data, filename)
 
 
@@ -17,6 +17,6 @@ def load_and_deserialize(filename):
     """
     inside the load function
     """
-    with open(filename, mode='rb') as file:
+    with open(filename, 'rb') as file:
         result = pickle.load(filename)
     return result
