52ec61ae20617735cda3db7f18a3c9687cea90af
Semedw <smusazade2000@gmail.com>
2025-12-19 15:48:15 +0400

    salam

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- grant all privileges to users
-SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
