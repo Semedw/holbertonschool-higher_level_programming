77f99398985fb315643f8ee3f575ca72dc22296a
Semedw <smusazade2000@gmail.com>
2025-12-19 21:20:11 +0400

    added int

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-	id PRIMARY KEY AUTO_INCREMENT,
+	id int PRIMARY KEY AUTO_INCREMENT,
 	state_id int,
 	name varchar(256),
 	FOREIGN KEY (state_id) REFERENCES states(id)
