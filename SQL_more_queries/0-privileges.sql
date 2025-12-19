be15e5961e220c21b115d52413fe5d0b7cb1d5ef
Semedw <smusazade2000@gmail.com>
2025-12-19 15:46:05 +0400

    salam

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,2 @@
 -- grant all privileges to users
-REVOKE ALL PRIVILEGES ON *.* 
-TO 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
+SHOW GRANTS ON *.* FOR 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
