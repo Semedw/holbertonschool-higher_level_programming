63e14f5d18847dc0c33366cbaf9129adeebf274e
Semedw <smusazade2000@gmail.com>
2025-12-20 11:28:31 +0400

    genre name

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
 -- number of shows by genre
-SELECT tv_genres.title, COUNT(tv_genres.id) AS number_of_shows
+SELECT tv_genres.name, COUNT(tv_genres.id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id;
