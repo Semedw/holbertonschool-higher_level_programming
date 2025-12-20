10891ccae638088222b380e187727924b404b3f5
Semedw <smusazade2000@gmail.com>
2025-12-20 12:01:31 +0400

    salam

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- my genres
 SELECT tv_genres.name
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.id
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_show ON tv_show.id = tv_show_genres.id
 ORDER BY tv_genres.name ASC;
