e56675f0c959a1b05691645d50aafd855d2a85d1
Semedw <smusazade2000@gmail.com>
2025-12-20 10:59:38 +0400

    salam

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,4 @@
 -- genre id for all shows
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-OR tv_show_genres.genre_id = NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
