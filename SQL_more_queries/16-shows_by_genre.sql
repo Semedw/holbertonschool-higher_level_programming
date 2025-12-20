6de49ead5afb02104a7892b612f889aadd56822d
Semedw <smusazade2000@gmail.com>
2025-12-20 12:25:58 +0400

    salam

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- list shows and genres
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-FULL OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-FULL OUTER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
