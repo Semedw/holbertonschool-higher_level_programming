75c45560040ca0b727ac737ca2b75b996eec682e
Semedw <smusazade2000@gmail.com>
2025-12-20 11:32:34 +0400

    group by

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,5 @@
 -- number of shows by genre
 SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id;
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name;
