f54ca5e2364657e42e579c960023f3668411fb6e
Semedw <smusazade2000@gmail.com>
2025-12-20 10:21:22 +0400

    changed join clause

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,7 @@
 -- genre id by show
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres on tv_genres.id = tv_shows.id
+JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id 
+AND tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
